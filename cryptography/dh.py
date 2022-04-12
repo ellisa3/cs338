fcba6c2e7b64766aebfb5fc695ae3b089a9f25fb
ellsa3 <ellisa3@carleton.edu>
2022-04-12 11:26:03 -0500

    completed being-eve

---
+++ cryptography/dh.py
@@ -0,0 +1,14 @@
+for i in range(1,59):
+    if ((11**i)%59 == 57):
+        print("a: " , i)
+
+for i in range(1,59):
+    if ((11**i)%59 == 44):
+        print("b: ",i)
+
+
+print("___________________")
+
+for i in range(1,10000):
+    if ((13*i) % 5148 == 1):
+        print("d: ",i)
